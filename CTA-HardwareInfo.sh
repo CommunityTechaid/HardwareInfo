f120c75d66c6ed51c6be973e6db2cfe38b49ce7b
akhil <akhil@strider.cta.org>
2023-12-01 16:32:11 +0000

    Updated stuff in the script to get more hw info + added usage of two tools: acpitool, smartmontools (both available by default on ShredOS)

--- CTA-HardwareInfo.sh
+++ CTA-HardwareInfo.sh
@@ -17,6 +17,7 @@
 #    - health
 #
 ###
+# required tools: jq, lshw, acpitool, smartmontools 
 
 system_info=$(sudo lshw -json)
 
@@ -35,18 +36,26 @@ tpm_version=$(sudo cat /sys/class/tpm/tpm0/tpm_version_major)
 
 ram_installed=$(jq '.children[] | select(.id == "core").children[] | select(.id == "memory").size ' <<< "$system_info" | numfmt --to=iec)
 
-declare -a disk_list
-declare -A disk_array
+#declare -a disk_list
+declare -a disk_names
+declare -a disk_sizes
+declare -a is_hdd
+#declare -A disk_array
 
-disk_list+=($(lsblk -aAJ | jq '.blockdevices[] | select(.type="disk") | .name'))
+disk_info=$(lsblk --path -AdJo NAME,SIZE,ROTA)
+disk_names+=($(jq '.blockdevices[] | .name' <<< "$disk_info"))
+disk_sizes+=($(jq '.blockdevices[] | .size' <<< "$disk_info"))
+is_hdd+=($(jq '.blockdevices[] | .rota' <<< "$disk_info"))
 
-for i in $disk_list;
-    do 
-        size=$(lsblk -aAJ | jq '.blockdevices[] | select(.type="disk") | select(.name="$i") | .size')
-        disk_array[$i]+=$size
-    done
+#disk_list+=($(lsblk -aAJ | jq '.blockdevices[] | select(.type="disk") | .name'))
 
-disk_number=${#disk_list[@]}
+#for i in $disk_list;
+#    do 
+#        size=$(lsblk -aAJ | jq '.blockdevices[] | select(.type="disk") | select(.name="$i") | .size')
+#        disk_array[$i]+=$size
+#    done
+
+disk_number=$(jq length <<< "$disk_info")
 
 printf \
 "# System info #
@@ -64,17 +73,40 @@ TPM version: %s
 
 RAM: %s
 
-Number of disks: %s
-Disk devices: %s
 " \
 "$system_manufacturer" \
-"$system_serial_number" \
 "$system_model" \
 "$system_version" \
+"$system_serial_number" \
 "$cpu_type" \
 "$cpu_bits" \
 "$cpu_cores" \
 "$tpm_version" \
-"$ram_installed" \
-"$disk_number" \
-"$disk_list"
+"$ram_installed" 
+
+printf "Disk number : %s\n" "$disk_number"
+printf "\n\n# DISK INFO #\n"
+printf "%-15s %-10s %-10s \n" \
+	"NAME" "SIZE" "HDD"
+
+for ((i=1;i<=disk_number;i++)); do 
+	printf "%-15s %-10s %-10s \n" \
+		"$disk_names[$i]" \
+		"$disk_sizes[$i]" \
+		"$is_hdd[$i]"
+	done
+
+
+printf "\n\n# DISK HEALTH REPORTS #\n\n"
+for name in ${disk_names[@]};
+do 
+	#this command works in shell but cannot find device type when run through the script. 
+	disk_health=$(sudo smartctl -s on -a "$name")
+	printf "$disk_health"
+done
+
+printf "\n\n# BATTERY HEALTH #\n\n"
+battery_health=$(sudo acpitool -B)
+printf "%s" $battery_health 
+printf "\n\n"
+
