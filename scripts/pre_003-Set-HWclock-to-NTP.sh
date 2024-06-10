372ed4cccc88b121a3d6f9fa2378b7fdaecd35c1
Tom C <tom.cronin@communitytechaid.org.uk>
2024-06-10 16:59:02 +0100

    Added some extra comments to HWClock script to explain wh

--- scripts/pre_003-Set-HWclock-to-NTP.sh
+++ scripts/pre_003-Set-HWclock-to-NTP.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
 ###
+#
 # Script to sync hardware clock with NTP
+#
+# (Current set up won't allow automatic syncing. ntpd needs multiple servers to 
+# calculate drift / accuracy and only Theta / 10.0.0.1 is accessible over the
+# wiping network.)
+#
 ###
 
-# Stop existing NTP daemon
+# Stop existing NTP daemon to allow other connections on the ntp socket
 /etc/init.d/S49ntp stop
 
 # Run NTP with one shot settings
