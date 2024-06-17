b0c183f98c781047e115d3d26c3e1876170a09a0
Tom C <tom.cronin@communitytechaid.org.uk>
2024-06-17 16:29:47 +0100

    Removed erroneous options that were for different ntp version/progamme

--- scripts/pre_003-Set-HWclock-to-NTP.sh
+++ scripts/pre_003-Set-HWclock-to-NTP.sh
@@ -13,9 +13,7 @@
 /etc/init.d/S49ntp stop
 
 # Run NTP with one shot settings
-# -q - run once and quit (rather than forking into daemon)
-# -g - don't panic at large time differences
-ntpdate -qg 10.0.0.1
+ntpdate 10.0.0.1
 
 # Write to hardware clock
 hwclock -w
