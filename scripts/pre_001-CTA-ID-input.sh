98801efdcfb98a3dfac65a41893ae22646369257
Tom C <tom.cronin@communitytechaid.org.uk>
2024-06-17 14:35:54 +0100

    Renamed pre_002 to a more descriptive name. Changed pre_001 to take user input of CTA ID

---
+++ scripts/pre_001-CTA-ID-input.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/bash
+exec 3>&1
+
+dlg_backtitle="Community TechAid drive eraser (feat. ShredOS and nwipe)"
+
+dlg () {
+    dialog --no-cancel --colors --backtitle "${dlg_backtitle}" "$@"  2>&1 1>&3
+}
+
+get_device_id () {
+    DEV_ID=$(dlg --title 'Device ID' --inputbox  "Enter the device ID from the CTA sticker.\n\nIf it doesn't have one, enter 0000"  10 30)
+}
+
+
+check_id () {
+    if [[ ! $DEV_ID =~ ^[0-9]+$ ]]; then
+        dlg --title 'ID check'\
+            --msgbox "You have entered \Zb\Z1${DEV_ID}\Zn, which is not a proper ID number.\n\nTry again" 10 30
+        return 1
+    fi
+}
+
+
+confirm_id () {
+    dlg --title 'Confirmation'\
+        --yesno "You have entered \Zb\Z1${DEV_ID}\Zn\n\nIs this the correct device ID?" 10 30
+}
+
+until get_device_id && check_id && confirm_id; do :; done
+
+echo "$DEV_ID" > CTA_ID
+
+clear
+
+exec 3>&-
